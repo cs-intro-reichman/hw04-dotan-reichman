339167fd939ecfb2e5cb2fdecd751e811757283b
Dotan Reichman <dotanreichman@Dotans-MacBook-Pro.local>
2025-11-19 12:55:52 +0200

    Done

--- Primes.java
+++ Primes.java
@@ -1,5 +1,24 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int N=Integer.parseInt(args[0]);
+        int count=0;
+        boolean[] isPrime=new boolean[N];
+        for(int i=2;i< N;i++){
+            for(int j=2;j<N;j++){
+                if(i%j==0 && i!=j){
+                    isPrime[i]=false;
+                    break;
+                }
+                else{
+                    isPrime[i]=true;
+                }
+            }
+            if(isPrime[i]){
+                System.out.println(i);
+                count++;
+            }
+        }
+        double perce= (count*100)/N;
+        System.out.println("There are "+ count +" primes between 2 and "+N+" ("+perce+"% are primes)");
     }
 }
