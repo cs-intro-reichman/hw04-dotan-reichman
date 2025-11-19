97e8bb981b94bc10e33c7e306dc02c3c0604c3d0
Dotan Reichman <dotanreichman@Dotans-MacBook-Pro.local>
2025-11-19 17:29:07 +0200

    Final?

--- Primes.java
+++ Primes.java
@@ -3,6 +3,7 @@ public class Primes {
         int N=Integer.parseInt(args[0]);
         int count=0;
         boolean[] isPrime=new boolean[N];
+        System.out.println("Prime numbers up to "+N+":");
         for(int i=2;i< N;i++){
             for(int j=2;j<N;j++){
                 if(i%j==0 && i!=j){
