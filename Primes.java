85f09b2f09948a803fd5194601cf69f5c6886484
Dotan Reichman <dotanreichman@Dotans-MacBook-Pro.local>
2025-11-27 16:11:21 +0200

    got 1 error out of 3 sucsessful runs in github changed one thing lets see if it works now

--- Primes.java
+++ Primes.java
@@ -2,17 +2,15 @@ public class Primes {
     public static void main(String[] args) {
         int N=Integer.parseInt(args[0]);
         int count=0;
-        boolean[] isPrime=new boolean[N];
+        boolean[] isPrime=new boolean[N+1];
         System.out.println("Prime numbers up to "+N+":");
-        for(int i=2;i< N;i++){
+        for(int i=2;i<= N;i++){
             for(int j=2;j<N;j++){
                 if(i%j==0 && i!=j){
                     isPrime[i]=false;
                     break;
                 }
-                else{
                     isPrime[i]=true;
-                }
             }
             if(isPrime[i]){
                 System.out.println(i);
